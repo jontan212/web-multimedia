e6029ffeb5af27a541171cb28cb7c6efacd34d98
Jonathan <jontan212yt@gamil.com>
2022-04-11 21:53:02 +0200

    Tiempo actual de reproducción hecho

--- src/components/MediaPlayer/components/FooterOptions/FooterOptions.js
+++ src/components/MediaPlayer/components/FooterOptions/FooterOptions.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 const FooterOptions = ({ ref_video, ref_media }) => {
+  const [totalTime, setTotalTime] = useState(),
+    [currentTime, setCurrentTime] = useState();
+
+  useEffect(() => {
+    // TOTAL DURATION OF THE VIDEO
+    ref_video.current.onloadeddata = () =>
+      setTotalTime(convertion(ref_video.current.duration));
+
+    // CURRENT TIME OF THE VIDEO
+    setCurrentTime(convertion(ref_video.current.currentTime));
+    ref_video.current.ontimeupdate = () => {
+      setCurrentTime(convertion(ref_video.current.currentTime));
+    };
+  }, []);
+
+  // MUTED
   const muted = () => {
     if (!ref_video.current.muted) {
       ref_video.current.muted = true;
@@ -10,9 +26,9 @@ const FooterOptions = ({ ref_video, ref_media }) => {
     }
   };
 
-  const openFullScreen = () => {
+  // FULL SCREEN
+  const fullScreen = () => {
     if (!document.fullscreenElement) {
-      // ref_media.current.requestFullscreen();
       if (ref_media.current.requestFullscreen)
         ref_media.current.requestFullscreen();
       else if (ref_media.current.mozRequestFullScreen)
@@ -22,7 +38,6 @@ const FooterOptions = ({ ref_video, ref_media }) => {
       else if (ref_media.current.msRequestFullscreen)
         ref_media.current.msRequestFullscreen();
     } else {
-      // document.exitFullscreen();
       if (document.exitFullscreen) document.exitFullscreen();
       else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
       else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
@@ -30,14 +45,43 @@ const FooterOptions = ({ ref_video, ref_media }) => {
     }
   };
 
+  // CONVERTION TIME
+  const convertion = (seconds) => {
+    let time = ``;
+
+    const segundos = Math.round(seconds % 60);
+    time = segundos <= 9 ? "0" + segundos : segundos;
+
+    const minutos = Math.floor(seconds / 60) % 60;
+    time = minutos <= 9 ? "0" + minutos + ":" + time : minutos + ":" + time;
+
+    const horas = Math.floor(seconds / 3600);
+
+    return horas === 0
+      ? time
+      : (time = horas <= 9 ? "0" + horas + ":" + time : horas + ":" + time);
+  };
+
+  // POINTER
+  const pointer = (e) => {
+    console.log(e.clientX);
+    // console.log(e.offsetX);
+  };
+
   return (
     <div className="footer_options">
       <Btn src_img="./assets/volume.svg" alt_img="Volume" fc={muted}></Btn>
-      <div></div>
+      <div className="container-progress-bar">
+        <span>{currentTime}</span>
+        <div className="progress" onMouseMove={pointer}>
+          <div className="pointer"></div>
+        </div>
+        <span>{totalTime}</span>
+      </div>
       <Btn
         src_img="./assets/fullscreen.svg"
         alt_img="Volume"
-        fc={openFullScreen}
+        fc={fullScreen}
       ></Btn>
       <Btn src_img="./assets/settings.svg" alt_img="Volume"></Btn>
     </div>
