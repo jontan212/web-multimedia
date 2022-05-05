058aea903cfb3147c200de9f494f043ddb8ecba1
Jonathan <jontan212yt@gamil.com>
2022-05-05 23:19:29 +0200

    intento de fracaso de la barra de video

--- src/components/MediaPlayer/components/FooterOptions/FooterOptions.js
+++ src/components/MediaPlayer/components/FooterOptions/FooterOptions.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
 const FooterOptions = ({ ref_video, ref_media }) => {
+  const refInputRange = useRef();
   const [totalTime, setTotalTime] = useState(),
     [currentTime, setCurrentTime] = useState();
 
@@ -16,6 +17,24 @@ const FooterOptions = ({ ref_video, ref_media }) => {
       setCurrentTime(convertion(ref_video.current.currentTime));
     };
   }, []);
+
+  useEffect(() => {
+    // ON TIME UPDATE
+    ref_video.current.ontimeupdate = () => {
+      let percentage =
+        (100 * ref_video.current.currentTime) / ref_video.current.duration;
+      // console.log(percentage);
+      // 100 / 23
+      // 500 / x
+
+      refInputRange.current.setAttribute("value", (1000 * percentage)/ 100);
+
+      refInputRange.current.style.backgroundImage = `-webkit-gradient(linear, left top, right top,
+      color-stop(${percentage}%, #FFFFFF),
+      color-stop(${percentage}%, #9B9B9B)`;
+      refInputRange.current.style.backgroundImage = `-moz-linear-gradient(left center, #FFFFFF 0%, #FFFFFF ${percentage}%, #9B9B9B ${percentage}%, #9B9B9B 100%)`;
+    };
+  }, [ref_video]);
 
   // MUTED
   const muted = () => {
@@ -62,10 +81,22 @@ const FooterOptions = ({ ref_video, ref_media }) => {
       : (time = horas <= 9 ? "0" + horas + ":" + time : horas + ":" + time);
   };
 
-  // POINTER
-  const pointer = (e) => {
-    console.log(e.clientX);
-    // console.log(e.offsetX);
+  // INPUT RANGE
+  const range = (e) => {
+    let val = (e.target.value - e.target.min) / (e.target.max - e.target.min);
+    let percent = val * 100;
+    e.target.style.backgroundImage = `-webkit-gradient(linear, left top, right top,
+      color-stop(${percent}%, #FFFFFF),
+      color-stop(${percent}%, #9B9B9B)`;
+    e.target.style.backgroundImage = `-moz-linear-gradient(left center, #FFFFFF 0%, #FFFFFF ${percent}%, #9B9B9B ${percent}%, #9B9B9B 100%)`;
+  };
+
+  // ON TIME UPDATE
+  const timeUpdate = () => {
+    // ref_video.current.setAttribute('value', percentage);
+    // console.log(ref_video.current);
+    // ref_video.current.setAttribute('value', `${percentage}`);
+    // e.target.setAttribute('value', percentage);
   };
 
   return (
@@ -73,9 +104,27 @@ const FooterOptions = ({ ref_video, ref_media }) => {
       <Btn src_img="./assets/volume.svg" alt_img="Volume" fc={muted}></Btn>
       <div className="container-progress-bar">
         <span>{currentTime}</span>
-        <div className="progress" onMouseMove={pointer}>
-          <div className="pointer"></div>
-        </div>
+        <input
+          type="range"
+          min="0"
+          max="1000"
+          // defaultValue="0"
+          onMouseMove={range}
+          onChange={timeUpdate}
+          style={{
+            backgroundImage:
+              "-webkit-gradient(linear, 0% 0%, 100% 0%, from(rgb(255, 255, 255)), from(rgb(155, 155, 155)))",
+          }}
+          ref={refInputRange}
+        ></input>
+        {/* <div className="progress">
+          <div
+            className="pointer"
+            onMouseDown={pointer}
+            onMouseUp={des}
+            style={{ left: `${left}px` }}
+          ></div>
+        </div> */}
         <span>{totalTime}</span>
       </div>
       <Btn
